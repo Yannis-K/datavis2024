3ce8bb79feac06c126df951221b454a59d24d326
he-lau <he.laurent.1130@gmail.com>
2024-01-17 21:04:04 +0100

    Fetch csv method

--- script/modules/data.js
+++ script/modules/data.js
@@ -7,24 +7,41 @@ export function getAllDataFromCSV(csvFilePath) {
       const rows = csvData.split("\n");
       const header = rows[0].split(",");
 
+      //console.log("***HEADER", header);
+
+      let counter = 0;
+
       const data = rows.slice(1).map((row) => {
-        const columns = row.split(",");
+        // Use a regular expression to handle quoted fields with commas
+        const columns = row.match(/(?:[^,"]|"(?:\\.|[^"])*")+/g);
 
         // Check if the number of columns matches the header length
-        if (columns.length === header.length) {
-          return header.reduce((acc, key, index) => {
-            acc[key.trim()] = columns[index].trim();
+        if (columns && columns.length === header.length) {
+          const rowData = header.reduce((acc, key, columnIndex) => {
+            acc[key.trim()] = columns[columnIndex]
+              .trim()
+              .replace(/^"(.*)"$/, "$1");
             return acc;
           }, {});
+
+          // Log columns with the counter
+          //console.log(`Row Counter ${++counter}:`, rowData);
+
+          return rowData;
         } else {
-          console.error(`Row has an incorrect number of columns: ${row}`);
+          console.error(
+            `Row has an incorrect number of columns or is not formatted correctly: ${row}`
+          );
           return null; // or handle this case accordingly
         }
       });
 
-      return data.filter((item) => item !== null); // Remove entries with incorrect number of columns
+      const filteredData = data.filter((item) => item !== null); // Remove entries with incorrect number of columns
+
+      return filteredData;
     })
     .catch((error) => {
       console.error("Error fetching or parsing CSV data:", error);
+      throw error; // Rethrow the error for further handling if needed
     });
 }

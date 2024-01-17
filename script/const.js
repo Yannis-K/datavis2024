47645d573e8308ea9c0dca3bffc1dd2c565381c8
he-lau <he.laurent.1130@gmail.com>
2024-01-17 21:44:44 +0100

    Timeline chart v1

--- script/const.js
+++ script/const.js
@@ -6,4 +6,7 @@ const DATA =
   (await getAllDataFromCSV(DATASET_FILE_PATH).catch((error) =>
     console.error("Error:", error)
   )) || [];
-export { DATA };
+
+const CURRENT_YEAR = new Date().getFullYear();
+
+export { DATA, CURRENT_YEAR };

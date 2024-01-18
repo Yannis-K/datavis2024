8f8dccba8f6fc3f50d549b83d0b50485ba6f38d2
he-lau <he.laurent.1130@gmail.com>
2024-01-18 21:03:41 +0100

    Timeline chart v2

--- script/const.js
+++ script/const.js
@@ -1,6 +1,7 @@
 import { getAllDataFromCSV } from "./modules/data.js";
 
 const DATASET_FILE_PATH = "./dataset/nobel_with_image.csv";
+//const DATASET_FILE_PATH = "./dataset/nobel.csv";
 
 const DATA =
   (await getAllDataFromCSV(DATASET_FILE_PATH).catch((error) =>
